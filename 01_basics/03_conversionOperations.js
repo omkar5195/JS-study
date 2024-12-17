0fdd34e17a3cc10328ca6bfb1625fee6e356080a
omkar5195 <omkar.5195@gmail.com>
2024-12-17 07:09:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = 0
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let someNumber=33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
