f68a4962e7b88b1cee05b2b5f81eb453925bf4b1
omkar5195 <omkar.5195@gmail.com>
2024-12-17 06:54:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Omkar"
+let age = 29
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value/ empty value/ also a object
+// undefined =>
+// symbol => unique
+
+
+// object
